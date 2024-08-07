d18a7a8a15948d32c519e237ffa6c46567b0c4f3
parkjihwan <mankcom7708@gmail.com>
2024-08-07 17:55:11 +0900

    ci : cicd 기초 파일 생성
    nginx 403 에러 해결 후, controller 변경
    
    #29

--- src/main/java/com/example/backoffice/TempHome.java
+++ src/main/java/com/example/backoffice/TempHome.java
@@ -1,10 +1,10 @@
 package com.example.backoffice;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1")
 public class TempHome {
 
