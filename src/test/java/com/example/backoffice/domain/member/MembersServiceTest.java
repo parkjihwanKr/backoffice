0bce6330d337736fc6a738ca6658d835d57bdc2b
parkjihwan <mankcom7708@gmail.com>
2024-08-14 17:20:15 +0900

    test : Members test code
    MemberRepository Service test code 작성
    
    #103

--- src/test/java/com/example/backoffice/domain/member/MembersServiceTest.java
+++ src/test/java/com/example/backoffice/domain/member/MembersServiceTest.java
@@ -1,4 +1,14 @@
 package com.example.backoffice.domain.member;
 
+import com.example.backoffice.domain.member.entity.Members;
+import com.example.backoffice.global.security.MemberDetailsImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class MembersServiceTest {
+
+    private MemberDetailsImpl memberDetails;
+
 }
