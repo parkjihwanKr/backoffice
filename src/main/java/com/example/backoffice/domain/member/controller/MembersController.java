ac105614b887f3f4a5e671a3d410b13fd2808b58
parkjihwan <mankcom7708@gmail.com>
2024-10-21 20:07:08 +0900

    fix : Members
    맴버 <-> 알림 시스템 수정
    
    #124

--- src/main/java/com/example/backoffice/domain/member/controller/MembersController.java
+++ src/main/java/com/example/backoffice/domain/member/controller/MembersController.java
@@ -17,8 +17,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
