aaa4297158bad856ce24ce24bb36ce833bd14053
parkjihwan <mankcom7708@gmail.com>
2025-01-20 22:44:49 +0900

    fix : Answers
    불필요한 주석 제거
    
    #169

--- src/main/java/com/example/backoffice/domain/answer/converter/AnswersConverter.java
+++ src/main/java/com/example/backoffice/domain/answer/converter/AnswersConverter.java
@@ -3,9 +3,6 @@ package com.example.backoffice.domain.answer.converter;
 import com.example.backoffice.domain.answer.entity.Answers;
 import com.example.backoffice.domain.question.entity.Questions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AnswersConverter {
 
     public static Answers toEntity(Questions question, String answerText, Long number){
