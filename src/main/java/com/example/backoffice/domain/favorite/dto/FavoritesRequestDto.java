290522021d158f4fd646b0f0d88931c0ab6ce117
parkjihwan <mankcom7708@gmail.com>
2025-01-21 17:19:21 +0900

    feat : Favorites
    Favorites Swagger UI 추가
    
    #169

--- src/main/java/com/example/backoffice/domain/favorite/dto/FavoritesRequestDto.java
+++ src/main/java/com/example/backoffice/domain/favorite/dto/FavoritesRequestDto.java
@@ -1,11 +1,10 @@
 package com.example.backoffice.domain.favorite.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 public class FavoritesRequestDto {
 
@@ -13,6 +12,8 @@ public class FavoritesRequestDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "FavoritesRequestDto.CreateOneDto",
+            description = "즐겨찾기 하나 생성 요청 DTO")
     public static class CreateOneDto {
         private String url;
         private String description;
@@ -22,15 +23,9 @@ public class FavoritesRequestDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "FavoritesRequestDto.UpdateOneDto",
+            description = "즐겨찾기 하나 수정 요청 DTO")
     public static class UpdateOneDto {
         private String description;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class DeleteDto {
-        private List<Long> favoriteIdList;
     }
 }
